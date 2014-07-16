53f180d24d551d0ffa4337a0c0f40e25a68f4adc
Benjamin 'blindCoder' Schieder <benjamin@anderdonau.de>
2014-07-16 11:49:22 +0200

    ShortRange and Galactic Chart look much nicer and waste less screen space.

--- SpaceTrader/src/main/java/de/anderdonau/spacetrader/NavigationChart.java
+++ SpaceTrader/src/main/java/de/anderdonau/spacetrader/NavigationChart.java
@@ -32,20 +32,19 @@ import de.anderdonau.spacetrader.DataTypes.SolarSystem;
 @SuppressWarnings("UnusedDeclaration")
 public class NavigationChart extends View {
 	protected final Paint     paint           = new Paint();
-	public          int       Multiplicator   = 20;
-	public          int       WormholeOffset  = 4;
+	public float   Multiplicator   = 20;
+	public boolean mOffsetsDefined = false;
+	public int     textColor       = Color.BLACK;
 	protected       Main      main            = null;
 	protected       GameState mGameState      = null;
 	protected       int       mDrawWormhole   = -1;
 	protected       int       mSelectedSystem = -1;
-	protected int radius;
-	protected float   mOffsetX        = 0;
-	protected float   mOffsetY        = 0;
-	public    boolean mOffsetsDefined = false;
-	protected boolean isShortRange    = true;
-	protected float   mCurrentX       = 0;
-	protected float   mCurrentY       = 0;
-	public int textColor = Color.BLACK;
+	protected float radius;
+	protected float   mOffsetX     = 0;
+	protected float   mOffsetY     = 0;
+	protected boolean isShortRange = true;
+	protected float   mCurrentX    = 0;
+	protected float   mCurrentY    = 0;
 
 	public NavigationChart(Context context) {
 		super(context);
@@ -80,8 +79,8 @@ public class NavigationChart extends View {
 	public int getSystemAt(float posX, float posY) {
 		for (int i = 0; i < GameState.MAXSOLARSYSTEM; i++) {
 			SolarSystem s = mGameState.SolarSystem[i];
-			int x = s.x * Multiplicator + (int) mOffsetX;
-			int y = s.y * Multiplicator + (int) mOffsetY;
+			double x = s.x * Multiplicator + (int) mOffsetX;
+			double y = s.y * Multiplicator + (int) mOffsetY;
 
 			if (posX >= x - radius && posX <= x + radius) {
 				if (posY >= y - radius && posY <= y + radius) {
@@ -103,8 +102,8 @@ public class NavigationChart extends View {
 		posY -= mOffsetY; // to get the actual touch position relative to 0,0
 		for (int i = 0; i < GameState.MAXSOLARSYSTEM; i++) {
 			s = mGameState.SolarSystem[i];
-			int nDist = (int) (Math.pow(posX - s.x * Multiplicator, 2) + Math.pow(
-				posY - s.y * Multiplicator, 2));
+			int nDist = (int) (Math.pow(posX - CoordToPixel(s.x), 2) + Math.pow(posY - CoordToPixel(s.y),
+				2));
 			if (dist > nDist) {
 				dist = nDist;
 				retVal = i;
@@ -114,7 +113,7 @@ public class NavigationChart extends View {
 	}
 
 	public int getWormholeAt(float posX, float posY) {
-		int system = getSystemAt(posX - (radius * 2 + WormholeOffset), posY);
+		int system = getSystemAt(posX - (radius * 2), posY);
 		if (system < 0) {
 			return -1;
 		}
@@ -162,6 +161,12 @@ public class NavigationChart extends View {
 	}
 
 	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		super.onSizeChanged(w, h, oldw, oldh);
+		mOffsetsDefined = false;
+	}
+
+	@Override
 	public void onDraw(Canvas canvas) {
 		if (mGameState == null) {
 			return;
@@ -169,105 +174,76 @@ public class NavigationChart extends View {
 
 		SolarSystem CURSYSTEM = mGameState.SolarSystem[mGameState.Mercenary[0].curSystem];
 		SolarSystem s;
-		int x, y;
+		float x;
+		float y;
 
 		if (isShortRange) {
-			radius = Math.min(getWidth(), getHeight()) / 20;
-			Multiplicator = 20;
+			Multiplicator = Math.min(getHeight(), getWidth()) / (float) (mGameState.Ship
+				.GetFuelTanks() * 2 + 3);
 		} else {
-			Multiplicator = Math.max(getWidth() / GameState.GALAXYWIDTH,
-				getHeight() / GameState.GALAXYHEIGHT);
-			radius = Math.min(getWidth(), getHeight()) / 40;
+			Multiplicator = (float) getHeight() / (float) GameState.GALAXYHEIGHT;
+			//		  Multiplicator = (float) getWidth() / (float) GameState.GALAXYWIDTH;
 		}
+		radius = Multiplicator * 2;
 
 		if (mSelectedSystem < 0) {
 			mSelectedSystem = mGameState.Mercenary[0].curSystem;
 		}
 		s = mGameState.SolarSystem[mSelectedSystem];
-		x = s.x * Multiplicator;
-		y = s.y * Multiplicator;
+		x = CoordToPixel(s.x);
+		y = CoordToPixel(s.y);
 
 		if (!mOffsetsDefined) {
 			/* Short range chart always focuses on CURSYSTEM.
 			 * Long range chart may focus on mSelectedSystem by using "Find" button.
 			 */
 			if (!isShortRange) {
-				mOffsetX = -s.x * Multiplicator + getWidth() / 2;
-				mOffsetY = -s.y * Multiplicator + getHeight() / 2;
+				mOffsetX = -x + getWidth() / 2;
+				mOffsetY = -y + getHeight() / 2;
 			} else {
-				mOffsetX = -CURSYSTEM.x * Multiplicator + getWidth() / 2;
-				mOffsetY = -CURSYSTEM.y * Multiplicator + getHeight() / 2;
+				mOffsetX = -CoordToPixel(CURSYSTEM.x) + getWidth() / 2;
+				mOffsetY = -CoordToPixel(CURSYSTEM.y) + getHeight() / 2;
 			}
 			mOffsetsDefined = true;
 			sanitizeOffsets();
 		}
+
 		canvas.translate(mOffsetX, mOffsetY);
+
+		/*
+		paint.setColor(Color.LTGRAY);
+		paint.setStyle(Paint.Style.STROKE);
+		for (x=0; x<GameState.GALAXYWIDTH;x++){
+			for (y=0; y<GameState.GALAXYHEIGHT;y++){
+				canvas.drawRect(x*Multiplicator, y*Multiplicator, x*Multiplicator+radius*2, y*Multiplicator+radius*2, paint);
+			}
+		}
+		*/
+
 		paint.setTextSize(isShortRange ? 40 : 20);
 		paint.setTextAlign(Paint.Align.CENTER);
 		paint.setStyle(Paint.Style.FILL);
 
 		paint.setColor(this.textColor);
-		paint.setStrokeWidth(3);
+		paint.setStrokeWidth(2);
 
 		if (isShortRange) {
-			canvas.drawLine(x - (radius * 1.25f), y, x + (radius * 1.25f), y, paint);
-			canvas.drawLine(x, y - (radius * 1.25f), x, y + (radius * 1.25f), paint);
+			canvas.drawLine(x - (radius * 1.5f), y, x + (radius * 1.5f), y, paint);
+			canvas.drawLine(x, y - (radius * 1.5f), x, y + (radius * 1.5f), paint);
 		} else {
-			canvas.drawLine(x - (radius * 2), y, x + (radius * 2), y, paint);
-			canvas.drawLine(x, y - (radius * 2), x, y + (radius * 2), paint);
+			canvas.drawLine(x - (radius * 3), y, x + (radius * 3), y, paint);
+			canvas.drawLine(x, y - (radius * 3), x, y + (radius * 3), paint);
 		}
 
-		paint.setStrokeWidth(0);
-
-		for (int i = 0; i < GameState.MAXSOLARSYSTEM; i++) {
-			s = mGameState.SolarSystem[i];
-			x = s.x * Multiplicator;
-			y = s.y * Multiplicator;
-			if (i == mSelectedSystem) {
-				paint.setColor(Color.RED);
-			} else if (s.visited) {
-				paint.setColor(Color.BLUE);
-			} else {
-				paint.setColor(Color.GREEN);
-			}
-
-			canvas.drawCircle(x, y, radius, paint);
-
-			paint.setColor(this.textColor);
-			canvas.drawText(main.SolarSystemName[s.nameIndex], x, y - radius, paint);
-		}
-
-		for (int i = 0; i < GameState.MAXWORMHOLE; i++) {
-			s = mGameState.SolarSystem[mGameState.Wormhole[i]];
-
-			x = s.x * Multiplicator;
-			y = s.y * Multiplicator;
-			paint.setColor(this.textColor);
-			canvas.drawCircle(x + radius * 2 + 4, y, radius, paint);
-			for (int r = radius; r >= 0; r--) {
-				paint.setARGB(255, (255 / radius) * r, (255 / radius) * r, 0);
-				canvas.drawCircle(x + radius * 2 + WormholeOffset, y, r, paint);
-			}
-		}
-
-		if (mGameState.Ship.GetFuel() > 0) {
-			x = CURSYSTEM.x * Multiplicator;
-			y = CURSYSTEM.y * Multiplicator;
-			paint.setStyle(Paint.Style.STROKE);
-			paint.setColor(this.textColor);
-			paint.setStrokeWidth(5);
-			canvas.drawCircle(x, y, mGameState.Ship.GetFuel() * Multiplicator, paint);
-			paint.setStrokeWidth(0);
-		}
 		paint.setStyle(Paint.Style.FILL_AND_STROKE);
 
 		if (mGameState.TrackedSystem >= 0) {
 			s = mGameState.SolarSystem[mGameState.TrackedSystem];
 			int distToTracked = mGameState.RealDistance(CURSYSTEM, s);
 			if (distToTracked > 0) {
-				paint.setStrokeWidth(3);
-				canvas.drawLine(CURSYSTEM.x * Multiplicator, CURSYSTEM.y * Multiplicator,
-					s.x * Multiplicator, s.y * Multiplicator, paint);
+				paint.setStrokeWidth(isShortRange ? 5 : 2);
+				canvas.drawLine(CoordToPixel(CURSYSTEM.x), CoordToPixel(CURSYSTEM.y), CoordToPixel(s.x),
+					CoordToPixel(s.y), paint);
 				paint.setStrokeWidth(0);
 			}
 		}
@@ -281,15 +257,58 @@ public class NavigationChart extends View {
 			SolarSystem to = mGameState.SolarSystem[mGameState.Wormhole[mDrawWormhole]];
 			paint.setColor(this.textColor);
 			paint.setStrokeWidth(5);
-			canvas.drawLine(from.x * Multiplicator + radius * 2 + WormholeOffset, from.y * Multiplicator,
-				to.x * Multiplicator, to.y * Multiplicator, paint);
+			canvas.drawLine(CoordToPixel(from.x) + radius * 2, CoordToPixel(from.y), CoordToPixel(to.x),
+				CoordToPixel(to.y), paint);
+			paint.setStrokeWidth(0);
+		}
+
+		paint.setStrokeWidth(0);
+
+		for (int i = 0; i < GameState.MAXSOLARSYSTEM; i++) {
+			s = mGameState.SolarSystem[i];
+			x = CoordToPixel(s.x);
+			y = CoordToPixel(s.y);
+			if (i == mSelectedSystem) {
+				paint.setColor(Color.RED);
+			} else if (s.visited) {
+				paint.setColor(Color.BLUE);
+			} else {
+				paint.setColor(Color.GREEN);
+			}
+
+			canvas.drawCircle(x, y, radius, paint);
+
+			//			if (isShortRange) {
+			paint.setColor(this.textColor);
+			canvas.drawText(main.SolarSystemName[s.nameIndex], x, y - radius, paint);
+			//			}
+		}
+
+		for (int i = 0; i < GameState.MAXWORMHOLE; i++) {
+			s = mGameState.SolarSystem[mGameState.Wormhole[i]];
+
+			x = CoordToPixel(s.x) + radius * 2;
+			y = CoordToPixel(s.y);
+			paint.setColor(this.textColor);
+			canvas.drawCircle(x, y, radius, paint);
+			for (float r = radius; r >= 0; r--) {
+				paint.setARGB(255, (int) ((255 / radius) * r), (int) ((255 / radius) * r), 0);
+				canvas.drawCircle(x, y, r, paint);
+			}
+		}
+
+		if (mGameState.Ship.GetFuel() > 0) {
+			x = CoordToPixel(CURSYSTEM.x);
+			y = CoordToPixel(CURSYSTEM.y);
+			paint.setStyle(Paint.Style.STROKE);
+			paint.setColor(this.textColor);
+			paint.setStrokeWidth(isShortRange ? 5 : 2);
+			canvas.drawCircle(x, y, mGameState.Ship.GetFuel() * Multiplicator, paint);
 			paint.setStrokeWidth(0);
 		}
 	}
 
-	@Override
-	protected void onSizeChanged (int w, int h, int oldw, int oldh){
-		super.onSizeChanged(w, h, oldw, oldh);
-		mOffsetsDefined = false;
+	public float CoordToPixel(int coord) {
+		return coord * Multiplicator;
 	}
 }
